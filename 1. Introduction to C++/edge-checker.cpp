0263e4186b8692ea5a4d3c12c9e1e0fad4f0e1af
Albert Zhou <abawwu.zhou@gmail.com>
2026-09-13 17:46:40 -0400

    edge-checker

---
+++ 1. Introduction to C++/edge-checker.cpp
@@ -0,0 +1,17 @@
+// https://atcoder.jp/contests/abc240/tasks/abc240_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b; cin >> a >> b;
+
+    if (abs(a%10 - b%10) == 1) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+
+    return 0;
+}
